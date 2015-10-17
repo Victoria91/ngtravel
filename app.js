c79313728c05d9692e912815e0ee4d129e080248
Victoria91 <vi4ka_91@mail.ru>
2015-10-18 01:05:54 +0300

    view list, simple adding of static data done

--- app.js
+++ app.js
@@ -2,4 +2,28 @@ var app = angular.module('Travel',[]);
 
 app.controller('TravelController', function($scope){
   $scope.title = 'Путешествия';
+
+  $scope.tours = [
+    {
+      title: 'Гостевой дом Лазурь 3*',
+      country: 'Россия, Краснодарский край, Сочи',
+      text: '3-я пляжная линиягалечный пляжбесплатный wi-fi в номерепостроен в 2002 году13 номеров',
+      price: 24864
+    },
+    {
+      title: 'Sevilla Palace 4*',
+      country: 'Мексика, Федеральный округ, Мехико',
+      text: 'бесплатный wi-fi в номерепостроен в 1988 году, 413 номеров, 23 этажа, лифт',
+      price: 236252
+    }
+  ]
+
+  $scope.addTour = function(){
+    $scope.tours.push({
+      title: 'Новый тур', 
+      country: 'Пока непонятно',
+      text: 'Да все ок будет)',
+      price: 201
+    });
+  }
 });
